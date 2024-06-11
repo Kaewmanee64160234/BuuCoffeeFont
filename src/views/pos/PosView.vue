245a4a94e2dbac8048a56d1f944bf22d59a23e7c
Kaewmanee <64160234@go.buu.ac.th>
2024-06-11 15:54:45 +0700

    chore: Refactor PosView.vue layout structure
    The code changes in PosView.vue refactor the layout structure to improve readability and maintainability. The changes include:
    - Wrapping the content in a v-sheet component for better visual separation
    - Moving the selected items list to a separate column for improved organization
    - Updating the styling of the v-container to match the background color
    
    These changes enhance the overall structure and appearance of the PosView component.

--- src/views/pos/PosView.vue
+++ src/views/pos/PosView.vue
@@ -1,19 +1,32 @@
 <template>
-    <v-app>
-      <v-container>
-        <promotion-cards-carousel></promotion-cards-carousel>
-        <search-bar></search-bar>
-        <category-tabs></category-tabs>
-        <v-row>
-          <v-col v-for="product in filteredProducts" :key="product.productId" cols="12" md="4">
-            <product-card :product="product"></product-card>
-          </v-col>
-        </v-row>
-        <selected-items-list></selected-items-list>
-        <drink-selection-dialog></drink-selection-dialog>
-      </v-container>
-    </v-app>
-  </template>
+  <v-app>
+    <v-row no-gutters>
+      <v-col>
+        <v-sheet class="pa-2 ma-2">
+          <v-container>
+            <promotion-cards-carousel></promotion-cards-carousel>
+            <search-bar></search-bar>
+            <category-tabs></category-tabs>
+            <v-row>
+              <v-col v-for="product in filteredProducts" :key="product.productId" cols="12" md="4">
+                <product-card :product="product"></product-card>
+              </v-col>
+            </v-row>
+            <drink-selection-dialog></drink-selection-dialog>
+          </v-container>
+        </v-sheet>
+      </v-col>
+      <v-col cols="3">
+        <v-sheet class="pa-2 ma-2">
+          <selected-items-list></selected-items-list>
+
+        </v-sheet>
+      </v-col>
+
+    </v-row>
+
+  </v-app>
+</template>
 
 <script lang="ts" setup>
 import { computed, onMounted } from 'vue';
@@ -34,24 +47,24 @@ const promotionStore = usePromotionStore();
 const toppingStore = useToppingStore();
 
 const filteredProducts = computed(() => {
-    return productStore.products.filter(product => {
-        const matchesCategory = productStore.selectedCategory ? product.category.categoryName === productStore.selectedCategory : true;
-        const matchesSearch = productStore.searchQuery ? product.productName.toLowerCase().includes(productStore.searchQuery.toLowerCase()) : true;
-        return matchesCategory && matchesSearch;
-    });
+  return productStore.products.filter(product => {
+    const matchesCategory = productStore.selectedCategory ? product.category.categoryName === productStore.selectedCategory : true;
+    const matchesSearch = productStore.searchQuery ? product.productName.toLowerCase().includes(productStore.searchQuery.toLowerCase()) : true;
+    return matchesCategory && matchesSearch;
+  });
 });
 
 onMounted(async () => {
-    await productStore.getAllProducts();
-    await categoryStore.getAllCategories(); 
-    await promotionStore.getAllPromotions();
-    await toppingStore.getAllToppings();
+  await productStore.getAllProducts();
+  await categoryStore.getAllCategories();
+  await promotionStore.getAllPromotions();
+  await toppingStore.getAllToppings();
 });
 </script>
 
 <style scoped>
 .v-container {
-    background-color: #faf4ee;
-    /* Match the background color */
+  background-color: #faf4ee;
+  /* Match the background color */
 }
 </style>
