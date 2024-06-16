74fdfdcff0716bd966dfc6200abb13681e5e3d3d
Cream260 <64160260@go.buu.ac.th>
2024-06-16 23:54:26 +0700

    logIn

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -30,43 +30,100 @@ const loginHandler = async () => {
   }
 };
 </script>
+
 <template>
-  <v-container>
-    <v-row justify="center">
-      <v-col cols="12" sm="8" md="4">
-        <v-card>
-          <v-card-title class="headline">Login</v-card-title>
-          <v-card-text>
-            <v-form ref="form" v-model="valid" lazy-validation>
-              <v-text-field
-                v-model="email"
-                :rules="emailRules"
-                label="Email"
-                required
-              ></v-text-field>
-              <v-text-field
-                v-model="password"
-                :rules="passwordRules"
-                label="Password"
-                type="password"
-                required
-              ></v-text-field>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="primary" @click="loginHandler">Login</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+  <v-container class="fill-height d-flex align-center justify-center background-image" fluid>
+    <v-card class="mx-auto my-12 pa-6" width="450" elevation="10">
+      <v-row justify="center">
+        <img src="../components/img/logo.jpg" alt="Logo" class="logo" style="width: 40%; height: 40%;">
+      </v-row>
+      <v-card-text>
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="ชื่อผู้ใช้"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            :rules="passwordRules"
+            label="รหัสผ่าน"
+            type="password"
+            required
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-card-actions class="d-flex justify-center">
+        <v-btn @click="loginHandler" class="login-btn" >
+          เข้าสู่ระบบ
+        </v-btn>
+      </v-card-actions>
+      
+      
+    </v-card>
+    <img src="../components/img/coffeeBaby1.png" alt="Character Top Left" class="character-top-left" >
+    <img src="../components/img/coffeeBaby2.png" alt="Character Bottom Right" class="character-bottom-right">
   </v-container>
 </template>
-
-
 
 <style scoped>
 .headline {
   text-align: center;
+  font-size: 24px;
+  margin-top: 10px;
+}
+
+.logo {
+  width: 80px;
+  height: 80px;
+}
+
+.v-btn {
+  width: 100%;
+}
+
+.fill-height {
+  height: 100vh;
+}
+
+.white--text {
+  color: #fff !important;
+}
+
+.background-image {
+  background: url('../components/img/bg.jpg') no-repeat center center;
+  background-size: cover;
+}
+
+.character-top-left {
+  position: absolute;
+  top: 50px; /* Adjust position */
+  left: 460px; /* Adjust position */
+  width: 200px; /* Adjust size as needed */
+  height: auto;
+  z-index: 1; /* Ensure it is above the card */
+}
+
+.character-bottom-right {
+  position: absolute;
+  bottom: 60px; /* Adjust position */
+  right: 450px; /* Adjust position */
+  width: 180px; /* Adjust size as needed */
+  height: auto;
+  z-index: 1; /* Ensure it is above the card */
+}
+
+.v-btn.login-btn {
+  width: 50%;
+  border: 2px solid #C5C5C5; /* เพิ่มเส้นขอบให้ปุ่ม */
+  color: #2B28A3;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+.v-btn.login-btn:hover {
+  background-color: #C5C5C5;
+  color: white;
 }
 </style>
