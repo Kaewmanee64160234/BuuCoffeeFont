c3c59cc563b64176045566e3f88a8237e36925aa
Kulisara Thongngamkhum <64160233@go.buu.ac.th>
2024-06-18 11:25:35 +0700

    chartSeries2

--- src/stores/report/expenditure.store.ts
+++ src/stores/report/expenditure.store.ts
@@ -1,22 +1,26 @@
-import { defineStore } from "pinia";
-import financeService from "@/service/report/finance.service";
+import { defineStore } from 'pinia';
+import financeService from '@/service/report/finance.service';
 
-export const useExpenditureStore = defineStore("expenditure", () => ({
-  startDate: "",
-  endDate: "",
-  totalExpenditure: 0,
-
-  async fetchExpenditure() {
-    try {
-      const res = await financeService.getExpenditure();
-
-          if (res.data) {
-            this.startDate = res.data.startDate;
-            this.endDate = res.data.endDate;
-            this.totalExpenditure = res.data.totalExpenditure;
-          }
-        } catch (error) {
-          console.error("Error fetching revenue:", error);
+export const useExpenditureStore = defineStore('expenditure', {
+  state: () => ({
+    startDate: '',
+    endDate: '',
+    totalExpenditureStartDate: 0,
+    totalExpenditureEndDate: 0,
+  }),
+  actions: {
+    async fetchExpenditure() {
+      try {
+        const res = await financeService.getExpenditure();
+        if (res.data) {
+          this.startDate = res.data.startDate;
+          this.endDate = res.data.endDate;
+          this.totalExpenditureStartDate = res.data.totalExpenditureStartDate;
+          this.totalExpenditureEndDate = res.data.totalExpenditureEndDate;
         }
-      },
-    }));
+      } catch (error) {
+        console.error('Error fetching expenditure:', error);
+      }
+    },
+  },
+});
