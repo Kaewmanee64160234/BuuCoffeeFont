bc63e19a21d2197371e936e396422c7944c1c6a4
Kaewmanee <64160234@go.buu.ac.th>
2024-08-12 00:12:07 +0700

    😅add validate promotion

--- src/components/promotions/CreatePromotionDialog.vue
+++ src/components/promotions/CreatePromotionDialog.vue
@@ -10,26 +10,17 @@
             <h3 class="text-h6">รายละเอียดโปรโมชั่น</h3>
             <br>
             <v-form ref="form1">
-              <v-text-field 
-                v-model="promotionName" 
-                label="ชื่อโปรโมชั่น" 
-                :rules="[ v => !!v || 'กรุณากรอกชื่อโปรโมชั่น',]"
-                required
-              ></v-text-field>
-              <v-text-field v-model="startDate" label="วันที่เริ่มต้น" type="date" :rules="[v => !!v || 'กรุณาเลือกวันที่เริ่มต้น']" required></v-text-field>
+              <v-text-field v-model="promotionName" label="ชื่อโปรโมชั่น"
+                :rules="[v => !!v || 'กรุณากรอกชื่อโปรโมชั่น',]" required></v-text-field>
+              <v-text-field v-model="startDate" label="วันที่เริ่มต้น" type="date"
+                :rules="[v => !!v || 'กรุณาเลือกวันที่เริ่มต้น']" required></v-text-field>
               <v-checkbox v-model="noEndDate" label="ไม่มีวันสิ้นสุด"></v-checkbox>
-              <v-text-field 
-                v-if="!noEndDate"
-                  v-model="endDate"
-                  label="วันที่สิ้นสุด"
-                  type="date"
-                  :rules="[ v => !!v || 'กรุณาเลือกวันที่สิ้นสุด']"
-                  :disabled="noEndDate"
-                  required
-              ></v-text-field>
+              <v-text-field v-if="!noEndDate" v-model="endDate" label="วันที่สิ้นสุด" type="date"
+                :rules="[v => !!v || 'กรุณาเลือกวันที่สิ้นสุด']" :disabled="noEndDate" required></v-text-field>
               <v-select v-model="promotionTypeName"
                 :items="promotionStore.promotionTypes.map(promotionType_ => promotionType_.text)" item-text="text"
-                item-value="value" label="ประเภทโปรโมชั่น" :rules="[v => !!v || 'กรุณาเลือกประเภทโปรโมชั่น']" required></v-select>
+                item-value="value" label="ประเภทโปรโมชั่น" :rules="[v => !!v || 'กรุณาเลือกประเภทโปรโมชั่น']"
+                required></v-select>
               <v-select v-model="promotionStore_" :items="store" item-text="text" item-value="value"
                 label="ร้านที่ใช้promotion" :rules="[v => !!v || 'กรุณาเลือกร้านที่ใช้โปรโมชั่น']" required></v-select>
               <v-checkbox v-model="promotionCanUseManyTimes" label="โปรโมชั่นนี้สามารถใช้ได้หลายครั้ง"></v-checkbox>
@@ -42,23 +33,31 @@
             <br>
             <v-form ref="form2">
               <template v-if="promotionTypeValue === 'discountPrice'">
-                <v-text-field v-model="discountValue" label="มูลค่าส่วนลด" type="number" :rules="[v => !!v || 'กรุณากรอกมูลค่าส่วนลด โดยกรอกเป็นตัวเลขเท่านั้น']" required></v-text-field>
+                <v-text-field v-model="discountValue" label="มูลค่าส่วนลด" type="number"
+                  :rules="[v => !!v || 'กรุณากรอกมูลค่าส่วนลด โดยกรอกเป็นตัวเลขเท่านั้น']" required></v-text-field>
               </template>
               <template v-if="promotionTypeValue === 'buy1get1'">
                 <v-autocomplete v-model="buyProductId"
                   :items="productStore.products.map(product => product.productName)" item-text="productName"
-                  item-value="id" label="ซื้อสินค้า" :rules="[v => !!v || 'กรุณาเลือกสินค้าที่ซื้อ']" required></v-autocomplete>
+                  item-value="id" label="ซื้อสินค้า" :rules="[v => !!v || 'กรุณาเลือกสินค้าที่ซื้อ']"
+                  required></v-autocomplete>
                 <v-autocomplete v-model="freeProductId"
                   :items="productStore.products.map(product => product.productName)" item-text="productName"
-                  item-value="id" label="สินค้าฟรี" :rules="[v => !!v || 'กรุณาเลือกสินค้าฟรี']" required></v-autocomplete>
+                  item-value="id" label="สินค้าฟรี" :rules="[v => !!v || 'กรุณาเลือกสินค้าฟรี']"
+                  required></v-autocomplete>
               </template>
               <template v-if="promotionTypeValue === 'usePoints'">
-                <v-text-field v-model="pointsRequired" label="คะแนนที่ต้องใช้" type="number" :rules="[v => !!v || 'กรุณากรอกคะแนนที่ต้องใช้ โดยกรอกเป็นตัวเลขเท่านั้น']" required></v-text-field>
-                <v-text-field v-model="discountValue" label="มูลค่าส่วนลด" type="number" :rules="[v => !!v || 'กรุณากรอกมูลค่าส่วนลด โดยกรอกเป็นตัวเลขเท่านั้น']" required></v-text-field>
+                <v-text-field v-model="pointsRequired" label="คะแนนที่ต้องใช้" type="number"
+                  :rules="[v => !!v || 'กรุณากรอกคะแนนที่ต้องใช้ โดยกรอกเป็นตัวเลขเท่านั้น']" required></v-text-field>
+                <v-text-field v-model="discountValue" label="มูลค่าส่วนลด" type="number"
+                  :rules="[v => !!v || 'กรุณากรอกมูลค่าส่วนลด โดยกรอกเป็นตัวเลขเท่านั้น']" required></v-text-field>
               </template>
               <template v-if="promotionTypeValue === 'discountPercentage'">
-                <v-text-field v-model="discountValue" label="เปอร์เซ็นต์ส่วนลด" type="number" :rules="[v => !!v || 'กรุณากรอกเปอร์เซ็นต์ส่วนลด โดยกรอกเป็นตัวเลขเท่านั้น']" required></v-text-field>
-                <v-text-field v-model="minimumPrice" label="ราคาขั้นต่ำสำหรับส่วนลด" type="number" :rules="[v => !!v || 'กรุณากรอกราคาขั้นต่ำสำหรับส่วนลด โดยกรอกเป็นตัวเลขเท่านั้น']" required></v-text-field>
+                <v-text-field v-model="discountValue" label="เปอร์เซ็นต์ส่วนลด" type="number"
+                  :rules="[v => !!v || 'กรุณากรอกเปอร์เซ็นต์ส่วนลด โดยกรอกเป็นตัวเลขเท่านั้น']" required></v-text-field>
+                <v-text-field v-model="minimumPrice" label="ราคาขั้นต่ำสำหรับส่วนลด" type="number"
+                  :rules="[v => !!v || 'กรุณากรอกราคาขั้นต่ำสำหรับส่วนลด โดยกรอกเป็นตัวเลขเท่านั้น']"
+                  required></v-text-field>
               </template>
             </v-form>
           </template>
@@ -137,9 +136,12 @@ const closeDialog = () => {
   // Reset validation for all forms
   const forms = document.querySelectorAll('form');
   forms.forEach(form => form.reset());
+  promotionStore.createPromotionDialog = false;
+
 };
 
 const createPromotion = async () => {
+  // General validation
   if (!promotionName.value || !promotionTypeValue.value || !startDate.value || (!noEndDate.value && !endDate.value)) {
     promotionStore.createPromotionDialog = false;
     Swal.fire({
@@ -154,6 +156,116 @@ const createPromotion = async () => {
     return;
   }
 
+  // Specific validation based on promotion type
+  if (promotionTypeValue.value === 'discountPrice' && discountValue.value === null) {
+    promotionStore.createPromotionDialog = false;
+
+    Swal.fire({
+      title: 'ข้อมูลไม่ครบถ้วน',
+      text: 'กรุณากรอกมูลค่าส่วนลด',
+      icon: 'error',
+      confirmButtonText: 'ตกลง',
+    }).then(() => {
+      promotionStore.createPromotionDialog = true;
+      step.value = 1;
+    });
+    return;
+  }
+
+  if (promotionTypeValue.value === 'buy1get1') {
+    if (!buyProductId.value) {
+      promotionStore.createPromotionDialog = false;
+
+      Swal.fire({
+        title: 'ข้อมูลไม่ครบถ้วน',
+        text: 'กรุณาเลือกสินค้าที่ซื้อ',
+        icon: 'error',
+        confirmButtonText: 'ตกลง',
+      }).then(() => {
+        promotionStore.createPromotionDialog = true;
+        step.value = 1;
+      });
+      return;
+    }
+    if (!freeProductId.value) {
+      promotionStore.createPromotionDialog = false;
+
+      Swal.fire({
+        title: 'ข้อมูลไม่ครบถ้วน',
+        text: 'กรุณาเลือกสินค้าฟรี',
+        icon: 'error',
+        confirmButtonText: 'ตกลง',
+      }).then(() => {
+        promotionStore.createPromotionDialog = true;
+        step.value = 1;
+      });
+      return;
+    }
+  }
+
+  if (promotionTypeValue.value === 'usePoints') {
+    if (pointsRequired.value === null) {
+      promotionStore.createPromotionDialog = false;
+
+      Swal.fire({
+        title: 'ข้อมูลไม่ครบถ้วน',
+        text: 'กรุณากรอกคะแนนที่ต้องใช้',
+        icon: 'error',
+        confirmButtonText: 'ตกลง',
+      }).then(() => {
+        promotionStore.createPromotionDialog = true;
+        step.value = 1;
+      });
+      return;
+    }
+    if (discountValue.value === null) {
+      promotionStore.createPromotionDialog = false;
+
+      Swal.fire({
+        title: 'ข้อมูลไม่ครบถ้วน',
+        text: 'กรุณากรอกมูลค่าส่วนลด',
+        icon: 'error',
+        confirmButtonText: 'ตกลง',
+      }).then(() => {
+        promotionStore.createPromotionDialog = true;
+        step.value = 1;
+      });
+      return;
+    }
+  }
+
+  if (promotionTypeValue.value === 'discountPercentage') {
+    if (discountValue.value === null) {
+      promotionStore.createPromotionDialog = false;
+
+      Swal.fire({
+        title: 'ข้อมูลไม่ครบถ้วน',
+        text: 'กรุณากรอกเปอร์เซ็นต์ส่วนลด',
+        icon: 'error',
+        confirmButtonText: 'ตกลง',
+      }).then(() => {
+        promotionStore.createPromotionDialog = true;
+        step.value = 1;
+      });
+      return;
+    }
+    if (minimumPrice.value === null) {
+      promotionStore.createPromotionDialog = false;
+
+      Swal.fire({
+        title: 'ข้อมูลไม่ครบถ้วน',
+        text: 'กรุณากรอกราคาขั้นต่ำสำหรับส่วนลด',
+        icon: 'error',
+        confirmButtonText: 'ตกลง',
+      }).then(() => {
+        promotionStore.createPromotionDialog = true;
+        step.value = 1;
+      });
+      return;
+    }
+  }
+
+  // If all validations pass, proceed to create the promotion
   const newPromotion: Promotion = {
     promotionName: promotionName.value,
     promotionType: promotionTypeValue.value,
@@ -161,7 +273,7 @@ const createPromotion = async () => {
     endDate: noEndDate.value ? null : new Date(endDate.value),
     discountValue: promotionTypeValue.value === 'discountPrice' || promotionTypeValue.value === 'usePoints' || promotionTypeValue.value === 'discountPercentage' ? discountValue.value : null,
     conditionQuantity: promotionTypeValue.value === 'usePoints' ? pointsRequired.value : null,
-    buyProductId: promotionTypeValue.value === 'buy1get1' ? productStore.products.find(product => product.productName === buyProductId.value)?.productId : null, 
+    buyProductId: promotionTypeValue.value === 'buy1get1' ? productStore.products.find(product => product.productName === buyProductId.value)?.productId : null,
     freeProductId: promotionTypeValue.value === 'buy1get1' ? productStore.products.find(product => product.productName === freeProductId.value)?.productId : null,
 
     conditionValue1: promotionTypeValue.value === 'discountPercentage' ? minimumPrice.value : null,
@@ -173,6 +285,7 @@ const createPromotion = async () => {
   };
 
   await promotionStore.createPromotion(newPromotion);
+  closeDialog(); // Close the dialog after the SweetAlert is dismissed
 
   Swal.fire({
     title: 'สำเร็จ',
@@ -182,10 +295,9 @@ const createPromotion = async () => {
     customClass: {
       confirmButton: 'swal-button'
     }
-  }).then(() => {
-    closeDialog(); // Close the dialog after the SweetAlert is dismissed
   });
 };
+
 </script>
 
 
